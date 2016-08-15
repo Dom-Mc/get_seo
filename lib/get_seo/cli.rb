d55c7f31643d0f47c79b00aceb9009a6d7afa182
Dom McKellar <dom.mckellar@gmail.com>
2016-08-15 15:53:53 -0700

    Add attr_readers for targeted attributes in scrape, use .setup to create new @seo instance and set attributes

--- lib/get_seo/cli.rb
+++ lib/get_seo/cli.rb
@@ -6,8 +6,8 @@ class GetSeo::CLI
   end
 
   def get_seo_data
-    url = "https://flatironschool.com" # TODO: Remove later
-    seo_info = GetSeo::Seo.setup(url)
+    # url = "https://flatironschool.com" # TODO: Remove later
+    seo_info = GetSeo::Seo.setup#(url)
   end
 
 end
--- lib/get_seo/seo.rb
+++ lib/get_seo/seo.rb
@@ -1,25 +1,34 @@
 class GetSeo::Seo
 
-  def self.setup(url)
+  attr_reader :heading1, :heading2, :heading3, :keyword, :title, :description, :alt_attribute
+  # NOTE: possibly add a 'url' attr_accessor later
+
+
+  def self.setup
+    # NOTE: setup Nokogiri object for scrape
+    url = "https://flatironschool.com" # TODO: Remove later
     html = Nokogiri::HTML(open(url))
 
-    header1 = html.search('h1').map do |h1|
+    # NOTE: instatiate a new object
+    @seo = self.new
+
+    @heading1 = html.search('h1').map do |h1|
       h1.text.strip
     end
 
-    header2 = html.search('h1').map do |h2|
+    @heading2 = html.search('h1').map do |h2|
       h2.text.strip
     end
 
-    header3 = html.search('h1').map do |h3|
+    @heading3 = html.search('h1').map do |h3|
       h3.text.strip
     end
 
-    description = html.at('meta[name="description"]')['content'] unless html.at('meta[name="description"]').nil?
+    @description = html.at('meta[name="description"]')['content'] unless html.at('meta[name="description"]').nil?
 
-    keywords = html.at('meta[name="keywords"]')['content'] unless html.at('meta[name="keywords"]').nil?
+    @keywords = html.at('meta[name="keywords"]')['content'] unless html.at('meta[name="keywords"]').nil?
 
-    binding.pry
+    # binding.pry
   end
 
 end
