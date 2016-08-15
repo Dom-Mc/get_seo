28777b7a9f84f15c581e47ec899d02c0030560e9
Dom McKellar <dom.mckellar@gmail.com>
2016-08-15 15:09:55 -0700

    Add #run method which will wrap main functionality

--- lib/get_seo/cli.rb
+++ lib/get_seo/cli.rb
@@ -1,5 +1,13 @@
-# Our CLI Controller
+#CLI Controller
 class GetSeo::CLI
 
+  def run
+    get_seo_data
+  end
+
+  def get_seo_data
+    url = "https://flatironschool.com" # TODO: Remove later
+    seo_info = GetSeo::Seo.setup(url)
+  end
 
 end
