e799ebe3469ab0f5653d7873027f317edc8fb9f6
Dom McKellar <dom.mckellar@gmail.com>
2016-08-15 15:12:18 -0700

    Create .setup method to create Nokogiri object and add calls to retrieve h1, h2, h3, keywords, and description info

--- lib/get_seo/seo.rb
+++ lib/get_seo/seo.rb
@@ -1,12 +1,24 @@
 class GetSeo::Seo
-  attr_accessor :url
-
-  def initialize(url)
-    @url = url
-  end
 
   def self.setup(url)
     html = Nokogiri::HTML(open(url))
+
+    header1 = html.search('h1').map do |h1|
+      h1.text.strip
+    end
+
+    header2 = html.search('h1').map do |h2|
+      h2.text.strip
+    end
+
+    header3 = html.search('h1').map do |h3|
+      h3.text.strip
+    end
+
+    description = html.at('meta[name="description"]')['content'] unless html.at('meta[name="description"]').nil?
+
+    keywords = html.at('meta[name="keywords"]')['content'] unless html.at('meta[name="keywords"]').nil?
+
     binding.pry
   end
 
