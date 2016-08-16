10747fa04ef55e3b59edd5b92f2b9e4663dc4bd1
Dom McKellar <dom.mckellar@gmail.com>
2016-08-16 11:05:23 -0700

    Add method to provide user list of options to choose from

--- lib/get_seo/cli.rb
+++ lib/get_seo/cli.rb
@@ -4,12 +4,17 @@ class GetSeo::CLI
   def run
     get_seo_data
     welcome_message
+    seo_options
     goodbye_message
   end
 
   def get_seo_data
     # url = "https://flatironschool.com" # TODO: Remove later
     seo_info = GetSeo::Seo.setup#(url)
+  end
+
+  def seo_options
+    puts "\nTo inspect a page's SEO information, please enter that page's domain (i.e. example.com):"
   end
 
   def welcome_message
