cd3cc182ae8440c76bc1555f1f58fe950fedbdb0
Dom McKellar <dom.mckellar@gmail.com>
2016-08-16 11:39:07 -0700

    Display a list of options for user

--- lib/get_seo/cli.rb
+++ lib/get_seo/cli.rb
@@ -18,6 +18,20 @@ class GetSeo::CLI
 
     seo_data = GetSeo::Seo.setup(requested_url)
     binding.pry
+
+
+    puts "Please choose from the following options:",
+     "-Display the 'Title' (Enter 1)",
+     "-Display the 'Description' (Enter 2)",
+     "-List the 'Keywords' (Enter 3)",
+     "-List the 'h1 Headings' (Enter 4)",
+     "-List the 'h2 Headings' (Enter 5)",
+     "-List the 'h3 Headings' (Enter 6)",
+     "-List the 'Alt Attributes' (Enter 7)",
+     "-List all SEO information from the page (Enter 8)",
+     "-Inspect a new page (Enter 9)",
+     "-Exit the program (Enter 'exit')"
+
   end
 
   def welcome_message
