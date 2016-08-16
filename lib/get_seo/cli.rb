26060b45401c32881a6f5168a939a8c1d2425002
Dom McKellar <dom.mckellar@gmail.com>
2016-08-16 10:57:23 -0700

    Create #goodbye_message to be displayed upon exiting the program

--- lib/get_seo/cli.rb
+++ lib/get_seo/cli.rb
@@ -4,6 +4,7 @@ class GetSeo::CLI
   def run
     get_seo_data
     welcome_message
+    goodbye_message
   end
 
   def get_seo_data
@@ -13,6 +14,10 @@ class GetSeo::CLI
 
   def welcome_message
     puts "Welcome to SEO. Ok enough chitchat, let's get to work!"
+  end
+
+  def goodbye_message
+    puts "Great SEO work! Job well done."
   end
 
 end
