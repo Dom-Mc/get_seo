ca6c020bc1e1c64a4c014e9b2f0f01166dd7c57d
Dom McKellar <dom.mckellar@gmail.com>
2016-08-16 16:51:48 -0700

    Create exception to protect against an invalid url

--- lib/get_seo/cli.rb
+++ lib/get_seo/cli.rb
@@ -17,8 +17,8 @@ class GetSeo::CLI
   def check_requested_protocal
     loop do
       puts "\nWhat does the webpage you would like to inspect use 'http' or 'https'?",
-           "-To select 'http' (Enter 1)",
-           "-To select 'https' (Enter 2)"
+      "-To select 'http' (Enter 1)",
+      "-To select 'https' (Enter 2)"
 
       requested_protocal = gets.strip.downcase
 
@@ -42,52 +42,60 @@ class GetSeo::CLI
     print protocal # 'http://' or 'https://'
     requested_url = gets.strip.downcase
 
-    #prvent program from running welcome_message
-    self.start_of_program = false
+    #ensure website can be accessed
+    begin
 
-    seo_data = GetSeo::Seo.setup(protocal + requested_url)
-    # binding.pry
+      #prvent program from running welcome_message
+      self.start_of_program = false
 
-  loop do
-    puts "\n\nPlease choose from the following options:",
-     "-Display the 'Title' (Enter 1)",
-     "-Display the 'Description' (Enter 2)",
-     "-List the 'Keywords' (Enter 3)",
-     "-List the 'h1 Headings' (Enter 4)",
-     "-List the 'h2 Headings' (Enter 5)",
-     "-List the 'h3 Headings' (Enter 6)",
-     "-List the 'Alt Attributes' (Enter 7)",
-     "-Inspect a new page (Enter 8)",
-     "-Exit the program (Enter 'exit')"
+      seo_data = GetSeo::Seo.setup(protocal + requested_url)
+    rescue
+      puts "\n\t*There seems to be an issue with the domain you entered.*",
+      "\t*Please check the website addresss and try again.*"
 
-     user_selection = gets.strip.downcase
+      self.run
+    else
+      loop do
+        puts "\n\nPlease choose from the following options:",
+        "-Display the 'Title' (Enter 1)",
+        "-Display the 'Description' (Enter 2)",
+        "-List the 'Keywords' (Enter 3)",
+        "-List the 'h1 Headings' (Enter 4)",
+        "-List the 'h2 Headings' (Enter 5)",
+        "-List the 'h3 Headings' (Enter 6)",
+        "-List the 'Alt Attributes' (Enter 7)",
+        "-Inspect a new page (Enter 8)",
+        "-Exit the program (Enter 'exit')"
 
-     case user_selection
-     when '1'
-       puts seo_data.title
-     when '2'
-       puts seo_data.description
-     when '3'
-       puts seo_data.keywords
-     when '4'
-       puts seo_data.heading1
-     when '5'
-       puts seo_data.heading2
-     when '6'
-       puts seo_data.heading3
-     when '7'
-       puts seo_data.alt_attribute
-     when '8'
-        self.run
-     when 'exit'
-       # NOTE: not sure about
-        # goodbye_message
-        break
-     else
-       puts "Sorry but #{user_selection} is invalid, please select another option.\n"
-     end
-   end
+        user_selection = gets.strip.downcase
 
+        case user_selection
+        when '1'
+          puts seo_data.title
+        when '2'
+          puts seo_data.description
+        when '3'
+          puts seo_data.keywords
+        when '4'
+          puts seo_data.heading1
+        when '5'
+          puts seo_data.heading2
+        when '6'
+          puts seo_data.heading3
+        when '7'
+          puts seo_data.alt_attribute
+        when '8'
+          self.run
+        when 'exit'
+          # NOTE: not sure about
+          # goodbye_message
+          break
+        else
+          puts "Sorry but #{user_selection} is invalid, please select another option.\n"
+        end
+      end
+
+    end
   end
 
   def welcome_message
