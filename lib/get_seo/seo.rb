a06425510cfc6b25037e94c64ef749ede289afad
Dom McKellar <dom.mckellar@gmail.com>
2016-08-15 20:08:40 -0700

    Change attr_readers to attr_accessors and set their values on .setup

--- lib/get_seo/seo.rb
+++ lib/get_seo/seo.rb
@@ -1,47 +1,50 @@
 class GetSeo::Seo
 
-  attr_reader :heading1, :heading2, :heading3, :keyword, :title, :description, :alt_attribute
+  attr_accessor :heading1, :heading2, :heading3, :keywords, :title, :description, :alt_attribute
   # NOTE: possibly add a 'url' attr_accessor later
+  # NOTE: create a method for iterating and printing attributes
 
-
-  def self.setup
+  def self.setup#(url)
     # NOTE: setup Nokogiri object for scrape
     url = "https://flatironschool.com" # TODO: Remove later
     html = Nokogiri::HTML(open(url))
 
-    # NOTE: instatiate a new object
-    @seo = self.new
+    # NOTE: instatiate a new seo object
+    seo = self.new
 
     # NOTE: #returns array of values || []
-    @heading1 = html.search("h1").map do |h1|
+    seo.heading1 = html.search("h1").map do |h1|
       h1.text.strip unless h1.text.strip.empty?
     end.compact
 
     # NOTE: #returns array of values || []
-    @heading2 = html.search("h2").map do |h2|
+    seo.heading2 = html.search("h2").map do |h2|
       h2.text.strip unless h2.text.strip.empty?
     end.compact
 
     # NOTE: #returns array of values || []
-    @heading3 = html.search("h3").map do |h3|
+    seo.heading3 = html.search("h3").map do |h3|
       h3.text.strip unless h3.text.strip.empty?
     end.compact
 
     # NOTE: #returns array of values || []
     if html.at("meta[name='description']").nil? || html.at("meta[name='description']")['content'].nil?
-      @description = []
+      seo.description = []
     else
-      @description = html.at("meta[name='description']")['content'].strip
+      seo.description = html.at("meta[name='description']")&['content'].strip
     end
 
     # NOTE: #returns array of values || []
     if html.at("meta[name='keywords']").nil? || html.at("meta[name='keywords']")['content'].nil?
-      @keywords = []
+      seo.keywords = []
     else
-      @keywords = html.at("meta[name='keywords']")['content'].strip
+      seo.keywords = html.at("meta[name='keywords']")&['content'].strip
     end
 
-    # binding.pry
+    binding.pry
+
+    #return our seo object
+    seo
   end
 
 end
