21f17f0b47f859d6ceac1ae6ddd710aa7e4c723a
Dom McKellar <dom.mckellar@gmail.com>
2016-08-17 20:26:00 -0700

    Rename methods for clarity and create seo_data attr_accessor to store seo instance

--- lib/get_seo/cli.rb
+++ lib/get_seo/cli.rb
@@ -1,7 +1,7 @@
 #CLI Controller
 class GetSeo::CLI
 
-  attr_accessor :protocal, :start_of_program
+  attr_accessor :protocal, :start_of_program, :seo_data
 
   def initialize
     self.start_of_program = true
@@ -10,66 +10,65 @@ class GetSeo::CLI
   def run
     welcome_message if start_of_program
     check_requested_protocal
-    seo_options
-    list_of_options
+    get_seo_info
+    seo_menu
     goodbye_message
   end
 
   private
 
-  def check_requested_protocal
-    loop do
-      print_line_break
-      puts "Check the webpage you would like to inspect and find out if it uses 'http' or 'https'?",
-      "-To select 'http' (Enter 1)",
-      "-To select 'https' (Enter 2)"
+    def check_requested_protocal
+      loop do
+        print_line_break
+        puts "Check the webpage you would like to inspect and find out if it uses 'http' or 'https'?",
+        "-To select 'http' (Enter 1)",
+        "-To select 'https' (Enter 2)"
 
-      requested_protocal = gets.strip.downcase
+        requested_protocal = gets.strip.downcase
 
-      if requested_protocal == '1' || requested_protocal == 'http'
-        self.protocal = 'http://'
-        break
-      elsif requested_protocal == '2' || requested_protocal == 'https'
-        self.protocal = 'https://'
-        break
-      else
-        section_break
-        puts "\t*Sorry but '#{requested_protocal}' is not a valid option.*"
-        section_break
+        if requested_protocal == '1' || requested_protocal == 'http'
+          self.protocal = 'http://'
+          break
+        elsif requested_protocal == '2' || requested_protocal == 'https'
+          self.protocal = 'https://'
+          break
+        else
+          section_break
+          puts "\t*Sorry but '#{requested_protocal}' is not a valid option.*"
+          section_break
+        end
       end
+
+      protocal
     end
 
-    protocal
-  end
+    def get_seo_info
+      print_line_break
+      puts "In order to retrieve SEO information, please enter the domain of the webpage you would like to inspect (i.e. example.com):"
 
-  def seo_options
-    print_line_break
-    puts "In order to retrieve SEO information, please enter the domain of the webpage you would like to inspect (i.e. example.com):"
+      print protocal # 'http://' or 'https://'
+      requested_url = gets.strip.downcase
 
-    print protocal # 'http://' or 'https://'
-    requested_url = gets.strip.downcase
+      # NOTE: Throw an exception if website cannote be accessed
+      begin
 
-    # NOTE: Throw an exception if website cannote be accessed
-    begin
+        # NOTE: pervent welcome_message from displaying more than once
+        self.start_of_program = false
 
-      # NOTE: pervent welcome_message from displaying more than once
-      self.start_of_program = false
+        self.seo_data = GetSeo::Seo.setup(protocal + requested_url)
 
-      @seo_data = GetSeo::Seo.setup(protocal + requested_url)
+      rescue
+        section_break
+        puts "\t*There seems to be an issue with the domain you entered.*",
+        "\t*Please double check the url and try again.*"
+        section_break
 
-    rescue
-      section_break
-      puts "\t*There seems to be an issue with the domain you entered.*",
-      "\t*Please double check the url and try again.*"
-      section_break
+        self.run
+      end
 
-      self.run
-    #   list_of_options
     end
 
-  end
-
-    def list_of_options
+    def seo_menu
       loop do
 
         print_line_break
@@ -88,19 +87,19 @@ class GetSeo::CLI
 
         case user_selection
         when '1'
-          print_seo_data(@seo_data.title, "Title Tag")
+          print_seo_data(seo_data.title, "Title Tag")
         when '2'
-          print_seo_data(@seo_data.description, "Description Meta Tag")
+          print_seo_data(seo_data.description, "Description Meta Tag")
         when '3'
-          print_seo_data(@seo_data.keywords, "Keywords Meta Tag")
+          print_seo_data(seo_data.keywords, "Keywords Meta Tag")
         when '4'
-          print_seo_data(@seo_data.heading1, "h1 Heading Tag(s)")
+          print_seo_data(seo_data.heading1, "h1 Heading Tag(s)")
         when '5'
-          print_seo_data(@seo_data.heading2, "h2 Heading Tag(s)")
+          print_seo_data(seo_data.heading2, "h2 Heading Tag(s)")
         when '6'
-          print_seo_data(@seo_data.heading3, "h3 Heading Tag(s)")
+          print_seo_data(seo_data.heading3, "h3 Heading Tag(s)")
         when '7'
-          print_seo_data(@seo_data.img_alt_attribute, "Image Alt Attribute(s)")
+          print_seo_data(seo_data.img_alt_attribute, "Image Alt Attribute(s)")
         else
           section_break
           puts "\t*Sorry but '#{user_selection}' is invalid, please select another option.*"
